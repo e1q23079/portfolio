9b439593ef1dcf799cf5550cee06d3dc7cdae156
e1q23079 <e1q23079@oit.ac.jp>
2025-03-26 01:55:22 +0900

    字体変更

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -8,8 +8,8 @@ function NotFound() {
       <div className="Center">
         <img src={Icon} width='200px' alt='icon'></img>
         <p className='notFound'>Not Found</p>
-        <p className='msg'>お探しのページは見つかりませんでした．</p>
-        <a href='#top' className='msg'>トップ</a>
+        <p className='message'>お探しのページは見つかりませんでした．</p>
+        <a href='#top' className='message'>トップ</a>
       </div>
     </>
   )
