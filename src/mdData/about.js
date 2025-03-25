709feda314b8dfce2d0f267945c14fe6ef462995
e1q23079 <e1q23079@oit.ac.jp>
2025-03-26 01:26:18 +0900

    プロフィール更新

--- src/mdData/about.js
+++ src/mdData/about.js
@@ -1,6 +1,12 @@
 const aboutText = `
 # About
 ***
+## プロフィール
+情報について学んでいる大学生です．
+## 所属
+- [大阪工業大学](https://www.oit.ac.jp/)
+- [HxSコンピュータ部](https://x.com/hxs_)
+- [MAGIOT](https://magiot.web.app/)
 `;
 
 export default aboutText;
