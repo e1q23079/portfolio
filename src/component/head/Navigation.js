57aeff32849d335761376d22546d7766affe75b6
e1q23079 <e1q23079@oit.ac.jp>
2025-03-25 16:11:30 +0900

    編集

--- src/component/head/Navigation.js
+++ src/component/head/Navigation.js
@@ -9,14 +9,14 @@ function Navigation() {
     <>
       <Navbar expand="lg" className="bg-body-tertiary" fixed='top'>
         <Container fluid>
-          <a href='#home'>
+          <a href='/#'>
             <img className='logo' src={logo} alt='logo' height='50px'></img>
           </a>
-          <Navbar.Brand href="#home">e1q23079's portfolio</Navbar.Brand>
+          <Navbar.Brand href="/#">e1q23079's portfolio</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="/#">Home</Nav.Link>
               <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#works">Works</Nav.Link>
               <Nav.Link href="#links">Links</Nav.Link>
