1d1a2369863f6171f698689cf5a10a2ffa57eabb
e1q23079 <e1q23079@oit.ac.jp>
2025-03-27 02:59:48 +0900

    アニメーションを追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Navigation from './component/Navigation/Navigation';
 
@@ -10,27 +10,32 @@ import Home from './component/Home/Home';
 
 import Md from './component/Md/Md';
 
-import {getAbout,getWorks,getLinks, getProducts} from './component/mdDt';
+import { getAbout, getWorks, getLinks, getProducts } from './component/mdDt';
 
 import NotFound from './component/NotFound/NotFound';
 
+import { AnimatePresence } from 'framer-motion';
+
 function App() {
+
+  const location = useLocation();
+
   return (
     <>
       <header>
         <Navigation />
       </header>
-      <HashRouter>
-        <Routes>
+      <AnimatePresence>
+        <Routes location={location} key={location.hash}>
           <Route exact path='/' element={<Home />} />
           <Route exact path='/home' element={<Home />} />
-          <Route exact path='/about' element={<Md text={getAbout()}/>} />
-          <Route exact path='/works' element={<Md text={getWorks()}/>} />
-          <Route exact path='/products' element={<Md text={getProducts()}/>} />
-          <Route exact path='/links' element={<Md text={getLinks()}/>} />
+          <Route exact path='/about' element={<Md text={getAbout()} />} />
+          <Route exact path='/works' element={<Md text={getWorks()} />} />
+          <Route exact path='/products' element={<Md text={getProducts()} />} />
+          <Route exact path='/links' element={<Md text={getLinks()} />} />
           <Route exact path='*' element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </AnimatePresence>
     </>
   );
 };
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -2,14 +2,16 @@ import './Home.css';
 
 import Icon from '../../icon.png';
 
+import { motion } from 'framer-motion';
+
 function Home() {
   return (
     <>
-      <div className="Center">
+      <motion.div className="Center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 0.1 }}>
         <img src={Icon} width='200px' alt='icon'></img>
         <p className='userName'>Q23079</p>
         <p className='userMessage'>色々なプログラムを書いている学生です！！</p>
-      </div>
+      </motion.div>
     </>
   )
 };
