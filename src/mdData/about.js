fd6e40d051bf30d51d373dd5e95ca0226f606d91
e1q23079 <e1q23079@oit.ac.jp>
2025-03-28 04:59:03 +0900

    about 更新

--- src/mdData/about.js
+++ src/mdData/about.js
@@ -2,11 +2,82 @@ const aboutText = `
 # About
 ***
 ## プロフィール
-情報について学んでいる大学生です．
+大学の情報科学部に在学しながら，C言語，Java，Python，JavaScriptを使って，Web開発やモバイル開発に挑戦してる学生です．
+
+新しい技術に触れることが好きで，それが楽しく，プログラミングを始めると，つい夢中になってしまいます．
+
+これまでに，RPG「幻宝伝説 関西に眠る記憶」，部室入退室管理システム，文化祭モバイルオーダーシステムなど，さまざまなプロジェクトを手掛けてきました．
+
+新しいことに挑戦する意欲を武器に，技術を磨きながら，使いやすく魅力的なシステムを開発できるエンジニアを目指しています！！
 ## 所属
 - [大阪工業大学](https://www.oit.ac.jp/)
+    - 大阪にある私立の工業系大学
+    - 情報科学部に所属
+    - プログラミング，アルゴリズム，コンピュータについて学ぶ
 - [HxSコンピュータ部](https://x.com/hxs_)
+    - ハードウェアとソフトウェアの両方に関する知識や技術を学び，実践するクラブ
 - [MAGIOT](https://magiot.web.app/)
+    - IT×リアル謎解きゲームを開発する学生プロジェクト
+- [バズるトップ](https://github.com/BazuruTop)
+    - 「バズるものを作ろう！！」という目標に向かって活動中！！
+
+## 活動歴
+- 大阪工業大学 情報科学部に入学
+- HxSコンピュータ部に入部
+- 学生プロジェクトMAGIOTに加入
+- 部内コンテスト2023で賞をゲット！！
+- IC科C演習の課題への取り組みの表彰（1位）
+- 謎解き学生プロジェクトの新メンバー教育の教材を作成
+- 「Name_not_set」（現：バズるトップ）」プロジェクトを始動！！
+- HxSコンピュータ部でFlaskの勉強会を主催
+- 部内コンテスト2024で賞をゲット！！
+- KC3 Hack2025（ハッカソン）に参加
+- 情報科学部長表彰賞受賞
+
+## 得意言語
+- C言語
+- Python
+- Java
+- JavaScript
+- Arduino言語
+## フロントエンド技術
+- HTML
+- CSS
+- Bootstrap
+- JavaScript
+- React
+- npm
+## バックエンド技術
+- Docker
+- Flask
+- MySQL
+- SQLite
+- Nginx
+- DiscordBot
+## 使用ツール
+- Git
+- GitHub
+- Visual Studio Code
+- Visual Studio
+- Android Studio
+- arduino IDE
+- Notinon
+- Canva
+- Figma
+- PowerShell
+- Postman
+- Designspark Mechanical
+- Autodesk Fusion
+## 環境
+- Linux
+- Ubuntu
+- Raspberrypi
+## その他
+- MarkDown
+- SVG
+- OpenCV
+- PyTorch
+- M5Stick
 `;
 
 export default aboutText;
