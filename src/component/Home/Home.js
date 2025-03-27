ec1434c2074341bc74193584047bab735746d05a
e1q23079 <e1q23079@oit.ac.jp>
2025-03-28 04:34:31 +0900

    ホームを修正

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -10,7 +10,7 @@ function Home() {
       <motion.div className="Center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 0.1 }}>
         <img src={Icon} width='200px' alt='icon'></img>
         <p className='userName'>Q23079</p>
-        <p className='userMessage'>日々挑戦し続ける，学生プログラマの記録</p>
+        <p className='userMessage'>日々挑戦し続ける，学生プログラマーの記録</p>
       </motion.div>
     </>
   )
