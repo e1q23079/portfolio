7bcedc6c60348ee74d0619677ac91856ce3619e9
e1q23079 <e1q23079@oit.ac.jp>
2025-03-28 15:43:31 +0900

    トップページ更新

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -7,10 +7,11 @@ import { motion } from 'framer-motion';
 function Home() {
   return (
     <>
-      <motion.div className="Center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 0.1 }}>
+      <motion.div initial={{ opacity: 1 }} animate={{ opacity: 0 }} transition={{ duration: 1, delay: 1 }}><div className='backGround'><div className='openMes'>日々挑戦し続ける，情報系学生の記録</div></div></motion.div>
+      <motion.div className="Center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 1 }}>
         <img src={Icon} width='200px' alt='icon'></img>
         <p className='userName'>Q23079</p>
-        <p className='userMessage'>日々挑戦し続ける，学生プログラマーの記録</p>
+        <p className='userMessage'>日々挑戦し続ける，情報系学生の記録</p>
       </motion.div>
     </>
   )
