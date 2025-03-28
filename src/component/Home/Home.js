383a80f185573ed82b2cf577a2d3031b85e380ed
e1q23079 <e1q23079@oit.ac.jp>
2025-03-28 22:51:12 +0900

    openアニメーション

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -7,8 +7,8 @@ import { motion } from 'framer-motion';
 function Home() {
   return (
     <>
-      <motion.div initial={{ opacity: 1 }} animate={{ opacity: 0 }} transition={{ duration: 1, delay: 1 }}><div className='backGround'><div className='openMes'>日々挑戦し続ける，情報系学生の記録</div></div></motion.div>
-      <motion.div className="Center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 1 }}>
+      <motion.div initial={{ opacity: 1 }} animate={{ opacity: 0 }} transition={{ duration: 1, delay: 4 }}><div className='backGround'><div className='openMes'>日々挑戦し続ける，情報系学生の記録</div></div></motion.div>
+      <motion.div className="Center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 4 }}>
         <img src={Icon} width='200px' alt='icon'></img>
         <p className='userName'>Q23079</p>
         <p className='userMessage'>日々挑戦し続ける，情報系学生の記録</p>
