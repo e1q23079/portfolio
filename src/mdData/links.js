8a1a57ceda0d9e70deed87d43c066f45381f0674
e1q23079 <e1q23079@oit.ac.jp>
2025-03-29 22:29:30 +0900

    links更新

--- src/mdData/links.js
+++ src/mdData/links.js
@@ -5,6 +5,10 @@ const linksText = `
 [https://github.com/e1q23079](https://github.com/e1q23079)
 ## Q23079's portfolio
 [https://e1q23079.github.io/portfolio/](https://e1q23079.github.io/portfolio/)
+## Q23079 自己紹介ページ
+[https://lss.oit.ac.jp/~e1q23079/](https://lss.oit.ac.jp/~e1q23079/)
+
+※閲覧には学内ネットワークまたはVPNの接続が必要です．
 `
 
 export default linksText;
