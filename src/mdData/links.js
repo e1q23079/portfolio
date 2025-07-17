f229dfbc1eadefbb3f649bca4db950b3d34bcff6
e1q23079 <e1q23079@oit.ac.jp>
2025-07-17 17:45:07 +0900

    links update

--- src/mdData/links.js
+++ src/mdData/links.js
@@ -3,7 +3,7 @@ const linksText = `
 ***
 ## ![](https://skillicons.dev/icons?i=github) GitHub
 [https://github.com/e1q23079](https://github.com/e1q23079)
-## Q23079's portfolio
+## ![](https://github.com/e1q23079/portfolio/blob/main/public/files/Q23079_icon.png?raw=true) Q23079's portfolio
 [https://e1q23079.github.io/portfolio/](https://e1q23079.github.io/portfolio/)
 ## Q23079 自己紹介ページ
 [https://lss.oit.ac.jp/~e1q23079/](https://lss.oit.ac.jp/~e1q23079/)
