7f222d7c957f8dd9c55d54451fee4aff76ce0f94
e1q23079 <e1q23079@oit.ac.jp>
2025-08-19 13:47:31 +0900

    URL追加

--- src/mdData/links.js
+++ src/mdData/links.js
@@ -24,6 +24,12 @@ https://e1q23079.vercel.app/
 #### | 「Flask勉強会」資料
 https://lss.oit.ac.jp/~e1q23079/web/10seconds/index.html
 
+#### | 「Flask勉強会」スライド資料
+https://lss.oit.ac.jp/~e1q23079/web/e-book/index.html?book=9aio20
+
+#### | 「Flask勉強会」スライド
+https://lss.oit.ac.jp/~e1q23079/web/Slide_Share_System/system/slide.html?10seconds
+
 #### | 「M5シリーズ×UDP勉強会」WEBページ
 https://lss.oit.ac.jp/~e1q23079/web/m5_zero/index.html
 
@@ -43,6 +49,9 @@ https://lss.oit.ac.jp/~e1q23079/web/HKTyping_pre/index.html
 
 #### | 「部内コンテスト2024」スライド
 https://lss.oit.ac.jp/~e1q23079/web/e-book/index.html?book=0xfa06
+
+#### | 「部内コンテスト2025」スライド
+https://lss.oit.ac.jp/~e1q23079/web/Slide_Share_System/system/live_shere?xhso2428
 
 #### | プログラミングとエンタメ要素
 https://lss.oit.ac.jp/~e1q23079/web/e-book/index.html?book=0xfb08
